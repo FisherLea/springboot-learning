dc5a4cc8820b5ad25c0cf74eb44957c7e0921197
yjscode@sina.cn <yuanjs1993>
2019-04-11 20:50:42 +0800

    modify channels

--- src/main/java/com/example/boot/MainApp.java
+++ src/main/java/com/example/boot/MainApp.java
@@ -1,6 +1,7 @@
 package com.example.boot;
 
 
+import com.example.boot.flume.FlumeAgentApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MainApp {
 
     public static void main(String[] args) {
+        FlumeAgentApplication.start("classpath:flume.properties");
 
         SpringApplication.run(MainApp.class, args);
     }
--- src/main/java/com/example/boot/flume/FlumeAgentApplication.java
+++ src/main/java/com/example/boot/flume/FlumeAgentApplication.java
@@ -1,8 +1,36 @@
 package com.example.boot.flume;
 
+import org.apache.flume.node.Application;
+import org.apache.flume.node.PropertiesFileConfigurationProvider;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.util.ResourceUtils;
+
+import java.io.File;
+
 public class FlumeAgentApplication {
 
-    public static void start(String propsFilePath){
+    private static final Logger logger = LoggerFactory.getLogger(FlumeAgentApplication.class);
 
+    public static final String AGENT_NAME = "agent_app";
+
+    public static void start(String propsFilePath){
+        try{
+            File file = ResourceUtils.getFile(propsFilePath);
+            PropertiesFileConfigurationProvider configurationProvider =
+                    new PropertiesFileConfigurationProvider(AGENT_NAME, file);
+            Application application = new Application();
+            application.handleConfigurationEvent(configurationProvider.getConfiguration());
+
+            application.start();
+            Runtime.getRuntime().addShutdownHook(new Thread("agent-shutdown-hook") {
+                @Override
+                public void run() {
+                    application.stop();
+                }
+            });
+        }catch (Exception e){
+            logger.error("A fatal error occurred while running. Exception follows.", e);
+        }
     }
 }
